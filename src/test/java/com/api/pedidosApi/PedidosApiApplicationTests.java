0427996667073b6bc75d5b1e6c2e2d641febcef3
janildoss <prjPedido_BackEnd_Spring>
2024-07-30 15:22:34 -0300

    projeto criado

---
+++ src/test/java/com/api/pedidosApi/PedidosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.pedidosApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
