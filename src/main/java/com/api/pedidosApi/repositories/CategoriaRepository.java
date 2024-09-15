d70fc10d3248313268e81d4f9db8badc81587907
janildoss <prjPedido_BackEnd_Spring>
2024-09-15 02:55:23 -0300

    adiciona categoria repository

--- src/main/java/com/api/pedidosApi/repositories/CategoriaRepository.java
+++ src/main/java/com/api/pedidosApi/repositories/CategoriaRepository.java
@@ -4,4 +4,5 @@ import com.api.pedidosApi.models.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
 }
