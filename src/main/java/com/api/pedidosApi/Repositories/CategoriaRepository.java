72300bba862905676ed4134ca5980f81a2ea2e0d
janildoss <prjPedido_BackEnd_Spring>
2024-09-19 10:13:21 -0300

    adiciona anotacao @Repository

--- src/main/java/com/api/pedidosApi/Repositories/CategoriaRepository.java
+++ src/main/java/com/api/pedidosApi/Repositories/CategoriaRepository.java
@@ -2,7 +2,9 @@ package com.api.pedidosApi.Repositories;
 
 import com.api.pedidosApi.models.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
 
 }
