b0d7b04b84cabf424f4273a6e898166d1a684662
janildoss <prjPedido_BackEnd_Spring>
2024-09-22 23:59:03 -0300

    ajustando o endpoint do produto

--- src/main/java/com/api/pedidosApi/controllers/ProdutoController.java
+++ src/main/java/com/api/pedidosApi/controllers/ProdutoController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value="/produtos")
 public class ProdutoController {
 
     @Autowired
