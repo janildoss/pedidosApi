1d10d493238514fa51ae3bb94c925f3fc347b19d
janildoss <prjPedido_BackEnd_Spring>
2024-11-09 22:00:18 -0300

    adiciona tiporecebimento repository

---
+++ src/main/java/com/api/pedidosApi/Repositories/TipoRecebimentoRepository.java
@@ -0,0 +1,8 @@
+package com.api.pedidosApi.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TipoRecebimentoRepository extends JpaRepository<TipoRecebimentoRepository, Integer> {
+}
