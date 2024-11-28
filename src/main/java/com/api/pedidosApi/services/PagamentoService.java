84c80ed5b318c5cfe2cac23d1f62e6dea37602d4
janildoss <prjPedido_BackEnd_Spring>
2024-11-28 11:38:34 -0300

    adiciona pagamento service

---
+++ src/main/java/com/api/pedidosApi/services/PagamentoService.java
@@ -0,0 +1,66 @@
+package com.api.pedidosApi.services;
+
+import com.api.pedidosApi.Repositories.PagamentoRepository;
+import com.api.pedidosApi.models.Categoria;
+import com.api.pedidosApi.models.Cliente;
+import com.api.pedidosApi.models.Pagamento;
+import com.api.pedidosApi.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PagamentoService {
+    @Autowired
+    private PagamentoRepository pagamentoRepository;
+
+    public List<Pagamento> findAll(){
+        return pagamentoRepository.findAll();
+    }
+
+    public Pagamento findById(Integer id) {
+        Optional<Pagamento> pagamento = pagamentoRepository.findById(id);
+        return pagamento.orElseThrow(() -> new ObjectNotFoundException(
+                "Pagamento não encontrada! Id: " + id + ", Tipo: " + Pagamento.class.getName()));
+    }
+
+    public Pagamento InsertPagamento (Pagamento pagamento){
+        pagamento.setId(null);
+        return pagamentoRepository.save(pagamento);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.READ_COMMITTED)
+    public void deletePagamento(Integer id){
+        if (!pagamentoRepository.existsById(id)) {
+            throw new ObjectNotFoundException("Pagamento não encontrada. Id: " + id);
+        }
+        pagamentoRepository.deleteById(id);
+    }
+
+    public Pagamento AtualizaPagamento(Integer Id){
+
+
+    }
+
+    public Pagamento updatePagamento(Integer id, Pagamento pagamentoAtualizado) {
+        Optional<Pagamento> pagamentoExistente = pagamentoRepository.findById(id);
+
+        if (pagamentoExistente.isPresent()) {
+            Pagamento pagamento = pagamentoExistente.get();
+            pagamento.setValor(pagamentoAtualizado.getValor() );
+            pagamento.setDataPagto(pagamentoAtualizado.getDataPagto());
+            pagamento.setIdPedido(pagamentoAtualizado.getIdPedido());
+            pagamento.setParcelaPagto(pagamentoAtualizado.getParcelaPagto());
+            pagamento.setTipoRecebimento(pagamentoAtualizado.getTipoRecebimento());
+            return pagamentoRepository.save(pagamentoAtualizado);
+        } else {
+            throw new ObjectNotFoundException("Categoria com id " + id + " não encontrada portanto não pode ser atualizada!");
+        }
+    }
+
+}
